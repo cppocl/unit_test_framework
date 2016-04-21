94ee07151bbbdde9fb450c46e6878b54afe67ef0
colinrg <colingirling@googlemail.com>
2016-04-21 22:53:33 +0100

    Created empty unit tests for all member functions of TestString class.

--- test/unit_tests/TestString.tests.cpp
+++ test/unit_tests/TestString.tests.cpp
@@ -19,6 +19,291 @@ limitations under the License.
 
 TEST_MEMBER_FUNCTION(TestString, TestString, char_const_ptr)
 {
-    ocl::TestString empty_string;
-    CHECK_TRUE(empty_string.IsEmpty());
+    using ocl::TestString;
+
+    TEST_OVERRIDE_ARGS("char const*");
+
+    {
+        TestString empty_string;
+        CHECK_TRUE(empty_string.IsEmpty());
+    }
+
+    {
+        TestString empty_string(NULL);
+        CHECK_TRUE(empty_string.IsEmpty());
+    }
+
+    {
+        TestString str("Hello");
+        CHECK_FALSE(str.IsEmpty());
+        CHECK_EQUAL(str.GetLength(), 5U);
+    }
+}
+
+TEST_MEMBER_FUNCTION(TestString, TestString, char_size_type)
+{
+    using ocl::TestString;
+
+    TEST_OVERRIDE_ARGS("char,size_type");
+
+}
+
+TEST_MEMBER_FUNCTION(TestString, TestString, TestString_const_ref)
+{
+    using ocl::TestString;
+
+    TEST_OVERRIDE_ARGS("TestString const&");
+
+}
+
+TEST_MEMBER_FUNCTION(TestString, operator_assignment, char_const_ptr)
+{
+    using ocl::TestString;
+
+    TEST_OVERRIDE_FUNCTION_NAME_ARGS("operator =", "char const*");
+
+}
+
+TEST_MEMBER_FUNCTION(TestString, operator_assignment, TestString_const_ref)
+{
+    using ocl::TestString;
+
+    TEST_OVERRIDE_FUNCTION_NAME_ARGS("operator =", "TestString const&");
+
+}
+
+TEST_MEMBER_FUNCTION(TestString, operator_plus_equal, TestString_const_ref)
+{
+    using ocl::TestString;
+
+    TEST_OVERRIDE_FUNCTION_NAME_ARGS("operator +=", "TestString const&");
+
+}
+
+TEST_CONST_MEMBER_FUNCTION(TestString, operator_subscript, size_type)
+{
+    using ocl::TestString;
+
+    TEST_OVERRIDE_FUNCTION_NAME("operator []");
+
+}
+
+TEST_MEMBER_FUNCTION(TestString, operator_subscript, size_type)
+{
+    using ocl::TestString;
+
+    TEST_OVERRIDE_FUNCTION_NAME("operator []");
+
+}
+
+TEST_CONST_MEMBER_FUNCTION(TestString, char_const_ptr, NA)
+{
+    using ocl::TestString;
+
+    TEST_OVERRIDE_FUNCTION_NAME("char const*");
+
+}
+
+TEST_CONST_MEMBER_FUNCTION(TestString, operator_is_equal, char_const_ptr)
+{
+    using ocl::TestString;
+
+    TEST_OVERRIDE_FUNCTION_NAME_ARGS("operator ==", "char const*");
+
+}
+
+TEST_MEMBER_FUNCTION(TestString, operator_is_equal, TestString_const_ref)
+{
+    using ocl::TestString;
+
+    TEST_OVERRIDE_FUNCTION_NAME_ARGS("operator ==", "TestString const&");
+
+}
+
+TEST_CONST_MEMBER_FUNCTION(TestString, Ptr, NA)
+{
+    using ocl::TestString;
+
+}
+
+TEST_CONST_MEMBER_FUNCTION(TestString, GetLength, NA)
+{
+    using ocl::TestString;
+
+}
+
+TEST_CONST_MEMBER_FUNCTION(TestString, IsEmpty, NA)
+{
+    using ocl::TestString;
+
+}
+
+TEST_MEMBER_FUNCTION(TestString, Clear, NA)
+{
+    using ocl::TestString;
+
+}
+
+TEST_CONST_MEMBER_FUNCTION(TestString, Find, char_size_type_ref_size_type)
+{
+    using ocl::TestString;
+
+    TEST_OVERRIDE_ARGS("char,size_type&,size_type");
+
+}
+
+TEST_MEMBER_FUNCTION(TestString, GetSubString, TestString_ref_size_type_size_type_bool)
+{
+    using ocl::TestString;
+
+    TEST_OVERRIDE_ARGS("TestString&,size_type,size_type,bool");
+
+}
+
+TEST_MEMBER_FUNCTION(TestString, Assign, char_const_ptr)
+{
+    using ocl::TestString;
+
+    TEST_OVERRIDE_ARGS("char const*");
+
+}
+
+TEST_MEMBER_FUNCTION(TestString, Assign, TestString_const_ref)
+{
+    using ocl::TestString;
+
+    TEST_OVERRIDE_ARGS("TestString const&");
+
+}
+
+TEST_MEMBER_FUNCTION(TestString, Move, char_ptr_ref_size_type)
+{
+    using ocl::TestString;
+
+    TEST_OVERRIDE_ARGS("char*&,size_type");
+
+}
+
+TEST_MEMBER_FUNCTION(TestString, Prepend, char_const_ptr)
+{
+    using ocl::TestString;
+
+    TEST_OVERRIDE_ARGS("char const*");
+
+}
+
+TEST_MEMBER_FUNCTION(TestString, Append, char_const_ptr)
+{
+    using ocl::TestString;
+
+    TEST_OVERRIDE_ARGS("char const*");
+
+}
+
+TEST_MEMBER_FUNCTION(TestString, Append, char_const_ptr_size_type)
+{
+    using ocl::TestString;
+
+    TEST_OVERRIDE_ARGS("char const*,size_type");
+
+}
+
+TEST_MEMBER_FUNCTION(TestString, Append, TestString_const_ptr)
+{
+    using ocl::TestString;
+
+    TEST_OVERRIDE_ARGS("TestString const&");
+
+}
+
+TEST_MEMBER_FUNCTION(TestString, Append, bool)
+{
+    using ocl::TestString;
+
+}
+
+TEST_MEMBER_FUNCTION(TestString, Append, char)
+{
+    using ocl::TestString;
+
+}
+
+TEST_MEMBER_FUNCTION(TestString, Append, char_size_type)
+{
+    using ocl::TestString;
+
+    TEST_OVERRIDE_ARGS("char,size_type");
+
+}
+
+TEST_MEMBER_FUNCTION(TestString, Append, char_size_type_char_const_ptr_size_type)
+{
+    using ocl::TestString;
+
+    TEST_OVERRIDE_ARGS("char,size_type,char const*,size_type");
+
+}
+
+TEST_MEMBER_FUNCTION(TestString, Append, signed_char_size_type)
+{
+    using ocl::TestString;
+
+    TEST_OVERRIDE_ARGS("signed char,size_type");
+
+}
+
+TEST_MEMBER_FUNCTION(TestString, Append, unsigned_char_size_type)
+{
+    using ocl::TestString;
+
+    TEST_OVERRIDE_ARGS("unsigned char,size_type");
+
+}
+
+TEST_MEMBER_FUNCTION(TestString, Append, signed_short_size_type)
+{
+    using ocl::TestString;
+
+    TEST_OVERRIDE_ARGS("signed short,size_type");
+
+}
+
+TEST_MEMBER_FUNCTION(TestString, Append, unsigned_short_size_type)
+{
+    using ocl::TestString;
+
+    TEST_OVERRIDE_ARGS("unsigned short,size_type");
+
+}
+
+TEST_MEMBER_FUNCTION(TestString, Append, signed_int_size_type)
+{
+    using ocl::TestString;
+
+    TEST_OVERRIDE_ARGS("signed int,size_type");
+
+}
+
+TEST_MEMBER_FUNCTION(TestString, Append, unsigned_int_size_type)
+{
+    using ocl::TestString;
+
+    TEST_OVERRIDE_ARGS("unsigned int,size_type");
+
+}
+
+TEST_MEMBER_FUNCTION(TestString, Append, signed_long_size_type)
+{
+    using ocl::TestString;
+
+    TEST_OVERRIDE_ARGS("signed long,size_type");
+
+}
+
+TEST_MEMBER_FUNCTION(TestString, Append, unsigned_long_size_type)
+{
+    using ocl::TestString;
+
+    TEST_OVERRIDE_ARGS("unsigned long,size_type");
+
 }
